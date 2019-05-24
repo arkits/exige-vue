abc040e40692f2e9f95d360f617b3d2a8ea73883
Khode, Archit (ARC-AFH)[UNIVERSITIES SPACE RESEARCH ASSN] <archit.khode@nasa.gov>
2019-05-23 20:46:34 -0700

    attempting to pass operations volumes to mapbox

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,8 +1,10 @@
 <template>
-    <mapbox 
-        v-bind:accessToken="mapboxAccessToken" 
-        v-bind:map-options="mapboxOptions"
-    />
+  <mapbox
+    v-bind:accessToken="mapboxAccessToken"
+    v-bind:map-options="mapboxOptions"
+    @map-load="mapLoaded"
+    @map-click="mapClicked"
+  />
 </template>
 
 <script>
@@ -10,8 +12,62 @@ import Mapbox from "mapbox-gl-vue";
 
 export default {
   name: "Map",
+  props: ["operations"],
   components: {
     mapbox: Mapbox
+  },
+  methods: {
+    mapLoaded(map) {
+        
+      console.log(this.operations);
+
+      var mapboxData = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [-77.03238901390978, 38.913188059745586]
+            },
+            properties: {
+              title: "Mapbox DC",
+              icon: "monument"
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [-122.414, 37.776]
+            },
+            properties: {
+              title: "Mapbox SF",
+              icon: "harbor"
+            }
+          }
+        ]
+      };
+
+      map.addLayer({
+        id: "points",
+        type: "symbol",
+        source: {
+          type: "geojson",
+          data: mapboxData
+        },
+        layout: {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        }
+      });
+    },
+    mapClicked(map, e) {
+      alert("Map Clicked!");
+    }
   },
   data() {
     return {
