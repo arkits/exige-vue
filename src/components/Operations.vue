19b2683f417b14f270fb2615101846c00f5d6b4e
Archit Khode <arkits@outlook.com>
2019-05-24 01:18:21 -0700

    made Operations card list vertically scrollable

--- src/components/Operations.vue
+++ src/components/Operations.vue
@@ -1,11 +1,13 @@
 <template>
 <v-container fluid grid-list-lg>
+    <div id="operations-cards-list">
     <div v-for="operation in operations" v-bind:key="operation.gufi">
         <v-layout row wrap>
             <v-flex xs12>
                 <OperationCard v-bind:operation="operation" />
             </v-flex>
         </v-layout>
+    </div>
     </div>
 </v-container>
 </template>
@@ -23,4 +25,8 @@ export default {
 </script>
 
 <style>
+#operations-cards-list{
+    overflow-y:auto;
+    height: 100vh;
+}
 </style>
