61c8f34caba0beafa0f9b056b26b122ea2342c12
Khode, Archit (ARC-AFH)[UNIVERSITIES SPACE RESEARCH ASSN] <archit.khode@nasa.gov>
2019-05-24 23:00:52 -0700

    updated ws-server.py to publish operation_volumes

--- ws-server.py
+++ ws-server.py
@@ -10,49 +10,47 @@ import json
 import uuid
 
 operation = {
-    "operation_geography":
-    {
+    "operation_volumes": [{
         "type": "Polygon",
-        "coordinates": [
-            [
-                [
-                    -122.51953124999999,
-                    35.17380831799959
-                ],
-                [
-                    -100.283203125,
-                    35.17380831799959
-                ],
-                [
-                    -100.283203125,
-                    45.42929873257377
-                ],
-                [
-                    -122.51953124999999,
-                    45.42929873257377
-                ],
-                [
-                    -122.51953124999999,
-                    35.17380831799959
+                "coordinates": [
+                    [
+                        [-118.91601562499999, 36.84446074079564],
+                        [-115.83984375, 36.84446074079564],
+                        [-115.83984375, 44.74673324024678],
+                        [-118.91601562499999, 44.74673324024678],
+                        [-118.91601562499999, 36.84446074079564]
+                    ]
                 ]
-            ]
-        ]
+    },
+        {
+        "type": "Polygon",
+                "coordinates": [
+                    [
+                        [-119.0478515625, 42.22851735620852],
+                        [-104.1064453125, 42.22851735620852],
+                        [-104.1064453125, 44.653024159812],
+                        [-119.0478515625, 44.653024159812],
+                        [-119.0478515625, 42.22851735620852]
+                    ]
+                ]
     }
+    ]
 }
 
 position = {}
+
 
 async def publish_operations(websocket, path):
     i = 1
 
     while True:
         # now = datetime.datetime.utcnow().isoformat() + 'Z'
-        
+
         gufi = str(uuid.uuid4())
         operation["gufi"] = gufi
         operation["uss_name"] = "uam.arc.nasa.gov"
         operation["state"] = "ACTIVE"
-        
+
         operation["id"] = i
         i = i + 1
 
@@ -62,18 +60,18 @@ async def publish_operations(websocket, path):
         await websocket.send(payload)
         await asyncio.sleep(5)
 
+
 async def publish_positions(websocket, path):
     i = 1
 
     while True:
         # now = datetime.datetime.utcnow().isoformat() + 'Z'
-        
+
         pos = [-122.019807, 45.632433]
         position["coods"] = pos
 
         gufi = str(uuid.uuid4())
         position["gufi"] = gufi
-        
 
         position["id"] = i
         i = i + 1
