21c626fc402acb9671cb8e85123a5f70c8d11221
Khode, Archit (ARC-AFH)[UNIVERSITIES SPACE RESEARCH ASSN] <archit.khode@nasa.gov>
2019-06-21 21:05:08 -0700

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ cd dist
 # Commit to Git
 
 git_commit_id=$(git rev-parse --short HEAD) 
-git_commit_message="New Deploy based on - $git_commit_id"
+git_commit_message="Deploy based on - $git_commit_id"
 
 git init
 git add -A
-git commit -m $git_commit_message
+git commit -m "$git_commit_message"
 
 # Push to gh-pages
 git push -f https://github.com/arkits/exige-vue.git master:gh-pages
