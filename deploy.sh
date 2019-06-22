86b07a7d7fc653576b1002c8f389f2ba69f5fb57
Khode, Archit (ARC-AFH)[UNIVERSITIES SPACE RESEARCH ASSN] <archit.khode@nasa.gov>
2019-06-21 21:02:34 -0700

    Update deploy.sh
    Generating better commit message

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,13 @@ npm run build
 cd dist
 
 # Commit to Git
+
+git_commit_id=$(git rev-parse --short HEAD) 
+git_commit_message="New Deploy based on - $git_commit_id"
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m $git_commit_message
 
 # Push to gh-pages
 git push -f https://github.com/arkits/exige-vue.git master:gh-pages
