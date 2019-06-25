a55e4570d48fa6bab383b6099280958ed47bc851
Archit Khode <arkits@outlook.com>
2019-06-24 23:40:40 -0700

    Added Raw Position array

--- src/components/OperationDetails/PositionsTimeline.vue
+++ src/components/OperationDetails/PositionsTimeline.vue
@@ -1,19 +1,37 @@
 <template>
-<v-timeline align-top dense>
-    <div v-for="(position, index) in getSocketPositionsForOperation" v-bind:key="index">
-    <v-timeline-item color="pink" small>
-        <v-layout pt-1>
-            <v-flex xs2>
-                 <v-icon dark>location_on</v-icon>
-            </v-flex>
+<div id="positionsDataView">
+    <div v-if="viewRawPositions">
+        <v-layout>
             <v-flex>
-                <strong>{{position.location.coordinates[0]}}, {{position.location.coordinates[1]}}</strong>
-                <div class="caption">{{position.uss_name}}</div>
+                <v-textarea outline height="50vh" name="raw-json" label="Raw Operation JSON" :value="positionsAsString"></v-textarea>
             </v-flex>
         </v-layout>
-    </v-timeline-item>
     </div>
-</v-timeline>
+    <div v-else>
+        <v-timeline align-top dense>
+            <div v-for="(position, index) in getSocketPositionsForOperation" v-bind:key="index">
+                <v-timeline-item color="pink" small>
+                    <v-layout pt-1>
+                        <v-flex xs2>
+                            <v-icon dark>location_on</v-icon>
+                        </v-flex>
+                        <v-flex>
+                            <strong>{{position.location.coordinates[0]}}, {{position.location.coordinates[1]}}</strong>
+                            <div class="caption">{{position.gufi}}</div>
+                            <div class="caption">{{position.uss_name}}</div>
+                        </v-flex>
+                    </v-layout>
+                </v-timeline-item>
+            </div>
+        </v-timeline>
+    </div>
+
+    <v-divider></v-divider>
+    <br>
+    <center>
+        <v-btn color="pink darken-1" @click="viewRawPositions = !viewRawPositions">View Raw</v-btn>
+    </center>
+</div>
 </template>
 
 <script>
@@ -26,12 +44,18 @@ export default {
     data() {
         return {
             dialog: false,
+            viewRawPositions: false,
             positions: []
         };
     },
     computed: {
         getSocketPositionsForOperation() {
-            return store.getters.getSocketPositionsForOperation(this.operation.gufi);
+            this.positions = store.getters.getSocketPositionsForOperation(this.operation.gufi);
+            return this.positions;
+        },
+        positionsAsString() {
+            var positionsString = JSON.stringify(this.positions, null, 4);
+            return positionsString;
         }
     }
 };
