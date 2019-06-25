50655e164263a77be3ee1e77dadc0a6e0b31e3e9
Archit Khode <arkits@outlook.com>
2019-06-24 23:40:09 -0700

    Minor UI Tweak - Changed color of Close button

--- src/components/OperationDetails/OperationDetailsDialog.vue
+++ src/components/OperationDetails/OperationDetailsDialog.vue
@@ -30,7 +30,7 @@
             </v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="green darken-1" @click="dialog = false">Close</v-btn>
+                <v-btn color="pink darken-1" @click="dialog = false">Close</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
